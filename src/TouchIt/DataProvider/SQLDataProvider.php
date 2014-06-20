83429656a7161efdaf38a8990f6efd692a6d0809
Marcus <zhouxule@icloud.com>
2014-06-20 23:05:38 +0800

    Update SQLDataProvider.php

--- src/TouchIt/DataProvider/SQLDataProvider.php
+++ src/TouchIt/DataProvider/SQLDataProvider.php
@@ -1,8 +1,11 @@
 <?php
-namespace TouchIt\DataProvider
+namespace TouchIt\DataProvider;
 
-use TouchIt\TouchIt
-use TouchIt\DataProvider\Provider
+use TouchIt\TouchIt;
+use TouchIt\DataProvider\Provider;
+use pocketmine\tile\Sign;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
 
 class SQLDataProvider implements Provider{
     private $database, $main, $lock;
@@ -13,7 +16,25 @@ class SQLDataProvider implements Provider{
         $this->loadDataBase();
     }
     
-    public function loadDataBase(){
+    public function getSign(Position $pos){
+        $level = $pos->getLevel();
+        if(!$level or ($level instanceof Level) === false)return false;
+        return true;
+    }
+    
+    public function addSign(Sign $sign){
+        $text = $sign->getText();
+        $level = $sign->getLevel();
+        if(!$level or ($level instanceof Level) === false)return false;
+        $this->database->exec("INSERT INTO sign(level, toLevel, x, y, z, hasDescription) VALUES ('".$level->getName()."', '".$text[2]."', ".(int) $sign->x.", ".(int) $sign->y.",".(int) $sign->z.", ".(($text[1] === "")?"0":"1").")");//Write to DataBase
+        if($text[1] !== ""){//Write description
+			$id = $this->database->query("SELECT id FROM sign WHERE hasDescription = 1 AND level = '".$level->getName()."' AND toLevel = '".$text[2]."' AND x = ".$sign->x." AND y = ".$sign->y." AND z = ".$sign->z.";")->fetchArray(SQLITE3_ASSOC)['id'];
+            $this->database->exec("INSERT INTO description(id, description) VALUES (".$id.", '".$text[1]."')");
+		}
+		return true;
+    }
+    
+    private function loadDataBase(){
         if(!extension_loaded("sqlite3")){
             $this->lock = true;
             return;
@@ -25,6 +46,8 @@ class SQLDataProvider implements Provider{
             @mkdir($this->main->getDataFolder());
             $this->database = new \SQLite3($this->main->getDataFolder(), SQLITE3_OPEN_READWRITE|SQLITE3_OPEN_CREATE);
         }
+        $this->database->exec("CREATE TABLE IF NOT EXISTS sign(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, level TEXT NOT NULL, toLevel TEXT NOT NULL, x INTEGER NOT NULL, y INTEGER NOT NULL, z INTEGER NOT NULL, hasDescription INTEGER DEFAULT 0)");
+        $this->database->exec("CREATE TABLE IF NOT EXISTS description(id INTEGER PRIMARY KEY NOT NULL, description STRING NOT NULL)");
     }
 }
 ?>
