8a1192cebbf826c8feda2e96aa123ae338eba31e
Marcus <zhouxule@icloud.com>
2014-06-20 23:15:52 +0800

    Update Provider.php

--- src/TouchIt/DataProvider/Provider.php
+++ src/TouchIt/DataProvider/Provider.php
@@ -1,3 +1,17 @@
 <?php
-namespace TouchIt\DataProvider
+namespace TouchIt\DataProvider;
+
+use TouchIt\TouchIt;
+use pocketmine\tile\Sign;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+
+interface Provider{
+    public function __construct(TouchIt $touchit);     //construct
+    public function getSign(Position $pos);            //To get sign from file
+    public function isSign(Position $pos);             //Check sign is teleport sign
+    public function removeSign(Position $pos);         //Remove the sign
+    public function addSign(Sign $sign);               //Add sign
+    public function close();                           //Close database or save config file
+}
 ?>
