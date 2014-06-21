0a777259dca83e3ec4997e4049e6f63e6034b46e
Marcus <zhouxule@icloud.com>
2014-06-21 16:46:38 +0800

    Update SignManager.php

--- src/TouchIt/SignManager.php
+++ src/TouchIt/SignManager.php
@@ -1,4 +1,18 @@
 <?php
+namespace TouchIt;
+
+use TouchIt\TouchIt;
+use TouchIt\Exchange\signInfo;
+use TouchIt\DataProvider\CNFDataProvider;
+use TouchIt\DataProvider\SQLDataProvider;
+
 class SignManager{
+    private $touchit, $config, $database;
+    
+    public function __construct(TouchIt $touchit, CNFDataProvider &$config, SQLDataProvider &$database){
+        $this->touchit = $touchit;
+        $this->config = $config;
+        $this->database = $database;
+    }
 }
 ?>
