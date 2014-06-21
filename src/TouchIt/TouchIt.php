14eedb469d01023cdae1e02418d385b5fa1753b2
Marcus <zhouxule@icloud.com>
2014-06-21 10:00:58 +0800

    Update TouchIt.php

--- src/TouchIt/TouchIt.php
+++ src/TouchIt/TouchIt.php
@@ -5,7 +5,9 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
 class TouchIt extends PluginBase implements Listener, CommandExecutor{
+    private $config;
     public function onLoad(){
+        
     }
 }
 ?>
