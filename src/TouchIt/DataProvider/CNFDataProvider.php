865ce53c04e57e3c3fb653b000e6ff20f44f121d
Marcus <zhouxule@icloud.com>
2014-06-21 09:36:37 +0800

    Update and rename CNFDataProvider to CNFDataProvider.php

---
+++ src/TouchIt/DataProvider/CNFDataProvider.php
@@ -0,0 +1,12 @@
+<?php
+namespace TouchIt\DataProvider;
+
+use TouchIt\TouchIt;
+use TouchIt\DataProvider\DataProvider;
+use pocketmine\tile\Sign;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+
+class CNFDataProvider implements DataProvider{
+}
+?>
