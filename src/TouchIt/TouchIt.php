c772528492385c2198c5354ee1f5cd424c03c69d
Marcus <zhouxule@icloud.com>
2014-06-21 13:17:18 +0800

    Update TouchIt.php

--- src/TouchIt/TouchIt.php
+++ src/TouchIt/TouchIt.php
@@ -5,12 +5,14 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use TouchIt\DataProvider\CNFDataProvider;
 use TouchIt\DataProvider\SQLDataProvider;
+use TouchIt\Listener\EventListener;
 
 class TouchIt extends PluginBase implements Listener, CommandExecutor{
-    public $config, $sign;
+    public $config, $sign, $listener;
     
     public function onLoad(){
-        $this->$config = new CNFDataProvider();
+        $this->$config = new CNFDataProvider($this, $this->getDataFolder()."Config.cnf");
+        $this->listener = new EventListener($this);
     }
 }
 ?>
