dbe993edb82dc303422bdcd4fa346618a85ebd5b
Marcus <zhouxule@icloud.com>
2014-06-21 09:38:05 +0800

    Update CNFDataProvider.php

--- src/TouchIt/DataProvider/CNFDataProvider.php
+++ src/TouchIt/DataProvider/CNFDataProvider.php
@@ -3,10 +3,12 @@ namespace TouchIt\DataProvider;
 
 use TouchIt\TouchIt;
 use TouchIt\DataProvider\DataProvider;
-use pocketmine\tile\Sign;
-use pocketmine\level\Level;
-use pocketmine\level\Position;
 
 class CNFDataProvider implements DataProvider{
+    private $touchit;
+    
+    public function __construct(TouchIt $touchit, $path){
+        
+    }
 }
 ?>
