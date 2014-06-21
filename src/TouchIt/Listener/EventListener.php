bdfcec1192ceb2650f559b5ca2d09ef77eb62b02
Marcus <zhouxule@icloud.com>
2014-06-21 16:49:02 +0800

    Update EventListener.php

--- src/TouchIt/Listener/EventListener.php
+++ src/TouchIt/Listener/EventListener.php
@@ -5,12 +5,14 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Server;
 use TouchIt\TouchIt;
+use TouchIt\SignManager;
 
 class EventListener implements Listener{
-    private $touchit;
+    private $touchit, $manager;
     
-    public function __construct(TouchIt $touchit){
+    public function __construct(TouchIt $touchit, SignManager $manager){
         $this->touchit = $touchit;
+        $this->manager = $manager;
     }
     
     public function onPlayerJoin(PlayerJoinEvent $event){
