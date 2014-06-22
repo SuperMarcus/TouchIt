1a8eba96042a0048a5ccd1dc08733a4bedc9e3b1
Marcus <zhouxule@icloud.com>
2014-06-22 10:51:47 +0800

    Update SignContentsData.php

--- src/TouchIt/Exchange/SignContentsData.php
+++ src/TouchIt/Exchange/SignContentsData.php
@@ -1,5 +1,28 @@
 <?php
-namespace TouchIt\Exchange
+namespace TouchIt\Exchange;
 
-class
+use TouchIt\Exchange\ExchangeInformation;
+use TouchIt\Exchange\SignData;
+
+class SignContentsData implements ExchangeInformation{
+    public $data;
+    public $database;
+    
+    public function __construct(\SQLite3 &$database){
+        $this->data = [];
+        &$this->database = $database;
+        $query = $database->query("SELECT id FROM sign;");
+        $this->data['id'] = [];
+        while($id = $query->fetchArray(SQLITE3_ASSOC)){
+            $this->data['id'][] = $query;
+        }
+        $this->data['count'] = count($this->data['id']);
+        $this->data['now'] = 0;
+    }
+    
+    public function getNext(){
+        if((++$this->data['now']) >= $this->data['count'])return false;
+        return new SignData($this->database->query("SELECT * FROM sign WHERE id = ".$this->data['now'].";"), $this->database);
+    }
+}
 ?>
