55439cea9daaa0d03dda8d8891119cd8219d7ba9
Marcus <zhouxule@icloud.com>
2014-06-27 23:52:10 +0800

    Update SignContentsData.php

--- src/TouchIt/Exchange/SignContentsData.php
+++ src/TouchIt/Exchange/SignContentsData.php
@@ -11,18 +11,30 @@ class SignContentsData implements ExchangeInformation{
     public function __construct(\SQLite3 &$database){
         $this->data = [];
         &$this->database = $database;
-        $query = $database->query("SELECT id FROM sign;");
+        $query = $database->query("SELECT * FROM sign;");
         $this->data['id'] = [];
         while($id = $query->fetchArray(SQLITE3_ASSOC)){
-            $this->data['id'][] = $query;
+            $this->data['query'][] = $query;
         }
-        $this->data['count'] = count($this->data['id']);
         $this->data['now'] = 0;
     }
     
     public function getNext(){
-        if((++$this->data['now']) >= $this->data['count'])return false;
-        return new SignData($this->database->query("SELECT * FROM sign WHERE id = ".$this->data['now'].";"), $this->database);
+        if(!isset($this->data['query'][(++$this->data['now'])]))return false;
+        $now = $this->data['query'][$this->data['now']];
+        return new SignData($this->database->query("SELECT * FROM sign WHERE id = ".$this->data['query'][$now['id']].";"), $this->database);
+    }
+    
+    public function delete($ID){
+        foreach($this->data['query'] as $key => $query){
+            if($query['id'] = $ID){
+                unset($this->data['query'][$key]);
+                if($key <= $this->data['now']){
+                    --$this->data['now'];
+                }
+                return;
+            }
+        }
     }
 }
 ?>
