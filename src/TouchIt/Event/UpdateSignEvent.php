5736b4eaa54fbfe47b755274177082c1f7b5114e
Marcus <zhouxule@icloud.com>
2014-06-27 23:05:15 +0800

    Add getText(), getSign(), setText()

--- src/TouchIt/Event/UpdateSignEvent.php
+++ src/TouchIt/Event/UpdateSignEvent.php
@@ -1,2 +1,35 @@
 <?php
+namespace TouchIt/Event;
+
+use pocketmine\event\Event;
+use TouchIt\Exchange\SignData;
+
+class UpdateSignEvent extends PluginEvent implements Cancellable{
+    private $textData, $sign;
+    
+    public function __construct(TouchIt $plugin, SignData $sign, $textData){
+        $this->plugin = $plugin;
+        $this->textData = $textData;
+        $this->sign = $sign;
+    }
+    
+    public function getSign(){
+        return $this->sign;
+    }
+    
+    public function getText(){
+        return $this->textData;
+    }
+    
+    public function setText($line1 = false, $line2 = false, $line3 = false, $line4 = false){
+        $oldData = $this->textData;
+        unset($this->textData);
+        $this->textData = [
+            (is_string($line1) or is_int($line1)) ? $oldData[0] : (string) $line1,
+            (is_string($line2) or is_int($line2)) ? $oldData[1] : (string) $line2,
+            (is_string($line3) or is_int($line3)) ? $oldData[2] : (string) $line3,
+            (is_string($line4) or is_int($line4)) ? $oldData[3] : (string) $line4,
+        ];
+    }
+}
 ?>
