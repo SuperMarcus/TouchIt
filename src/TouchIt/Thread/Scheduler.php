30e19d1fc567559c4e0a1774b2c4317fa090cdcb
Marcus <zhouxule@icloud.com>
2014-06-28 12:27:35 +0800

    Update Scheduler.php

--- src/TouchIt/Thread/Scheduler.php
+++ src/TouchIt/Thread/Scheduler.php
@@ -1,3 +1,11 @@
 <?php
-namespace TouchIt\scheduler;
+namespace TouchIt\thread;
+
+class scheduler extends \Thread{
+    public static $scheduler;
+    
+    public static function getScheduler(){
+        return self::$scheduler;
+    }
+}
 ?>
