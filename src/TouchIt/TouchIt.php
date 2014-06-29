9067ada9d49620c6641a0a432edce33eebb76c38
Marcus <zhouxule@icloud.com>
2014-06-29 15:19:44 +0800

    Update TouchIt.php

--- src/TouchIt/TouchIt.php
+++ src/TouchIt/TouchIt.php
@@ -3,24 +3,37 @@ namespace TouchIt;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 use TouchIt\DataProvider\CNFDataProvider;
 use TouchIt\DataProvider\SQLDataProvider;
 use TouchIt\Listener\EventListener;
 use TouchIt\SignManager;
 
-class TouchIt extends PluginBase implements Listener, CommandExecutor{
+class TouchIt extends PluginBase implements CommandExecutor{
     public $config, $database, $listener, $signManager;
     
-    public function onEnable(){
+    public function onLoad(){
         $this->config = new CNFDataProvider($this, $this->getDataFolder()."Config.cnf");
         $this->database = new SQLDataProvider($this);
         $this->signManager = new SignManager($this, $this->config, $this->database);
         $this->listener = new EventListener($this, $this->signManager);
     }
     
+    public function onEnable(){
+        $this->config->onEnable();
+        $this->database->onEnable();
+        $this->signManager->start()
+    }
+    
     public function onDisable(){
         $this->config->onDisable();
         $this->database->onDisable();
+        $this->signManager->stop();
     }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        return $this->signManager->onCommand(CommandSender $sender, Command $command, $label, array $args);
+	}
 }
 ?>
