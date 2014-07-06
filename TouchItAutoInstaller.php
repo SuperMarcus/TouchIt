e295867e6ad26144e5c4c38a1095ccac55729bcd
Marcus <zhouxule@icloud.com>
2014-07-06 14:33:53 +0800

    Update TouchItAutoInstaller.php

--- TouchItAutoInstaller.php
+++ TouchItAutoInstaller.php
@@ -1,2 +1,44 @@
 <?php
+/*
+__PocketMine Plugin__
+name=TouchItAutoInstaller
+description=TouchIt Auto Installer
+version=1.0
+apiversion=12,13
+author=Marcus
+class=TouchItAutoInstaller
+*/
+
+class TouchItAutoInstaller implements Plugin{
+    private $api;
+    public function __construct(ServerAPI $api, $server = false){}
+    public function __destruct(){}
+    
+    public function init(){
+        if(!Utils::isOnline())return;//check network
+        if(file_exists(DATA_PATH."plugins/.touchit")){
+            $local = trim(file_get_contents(DATA_PATH."plugins/.touchit"));
+            $online = json_decode(Utils::curl_get("https://api.github.com/repos/SuperMarcus/TouchIt/commits"), true);
+            if(!$online){
+                console("[TouchIt] Github API error.", true, true, 0);
+                return;
+            }
+            if($local != $online[0]["commit"]["committer"]["date"]){
+                console("[TouchIt] A new version of TouchIt has been found.");
+                file_put_contents(DATA_PATH."plugins/.touchit", $online[0]["commit"]["committer"]["date"]);
+                file_put_contents(DATA_PATH."plugins/touchit.php", Utils::curl_get("https://github.com/SuperMarcus/TouchIt/raw/master/TouchIt.php"));
+                $this->api->console->defaultCommands("stop", array(), "console", "stop");
+            }
+            return;
+        }
+        console("[TouchIt] Downloading TouchIt");
+        $online = json_decode(Utils::curl_get("https://api.github.com/repos/SuperMarcus/TouchIt/commits"), true);
+        if(!$online){
+            console("[TouchIt] Github API error.", true, true, 0);
+        }
+        file_put_contents(DATA_PATH."plugins/.touchit", $online[0]["commit"]["committer"]["date"]);
+        file_put_contents(DATA_PATH."plugins/touchit.php", Utils::curl_get("https://github.com/SuperMarcus/TouchIt/raw/master/TouchIt.php"));
+        $this->api->console->defaultCommands("stop", array(), "console", "stop");
+    }
+}
 ?>
