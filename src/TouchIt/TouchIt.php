a71c07a1d85961cc202fd4aa65a5259cc4a756ae
Marcus <zhouxule@icloud.com>
2014-07-17 18:21:22 +0800

    Update new format and remove onCommand() method.

--- src/TouchIt/TouchIt.php
+++ src/TouchIt/TouchIt.php
@@ -2,38 +2,99 @@
 namespace TouchIt;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandExecutor;
 use pocketmine\utils\TextFormat;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use TouchIt\DataProvider\CNFDataProvider;
+use TouchIt\DataProvider\Provider;
 use TouchIt\DataProvider\SQLDataProvider;
 use TouchIt\Listener\EventListener;
 use TouchIt\SignManager;
 
-class TouchIt extends PluginBase implements CommandExecutor{
-    public $config, $database, $listener, $signManager;
+class TouchIt extends PluginBase{
+    private $objects;
+    
+    /** @var SignManager */
+    public static $manager;
+    /** @var Provider */
+    public static $configProvider;
+    /** @var Provider */
+    public static $dataProvider;
+    /** @var EventListener */
+    public static $listener;
+    /** @var TouchIt */
+    public static $main;
     
     public function onLoad(){
-        $this->config = new CNFDataProvider($this, $this->getDataFolder()."Config.cnf");
-        $this->database = new SQLDataProvider($this);
-        $this->signManager = new SignManager($this, $this->config, $this->database);
-        $this->listener = new EventListener($this, $this->signManager);
+        $this->objects = [//The providers and managers
+            "manager" => new SignManager(),
+            "config" => new CNFDataProvider($this->getDataFolder()."Config.cnf"),
+            "data" => new SQLDataProvider(),
+            "listener" => new EventListener()
+        ];
+        
+        self::$manager = $this->objects["manager"];
+        self::$configProvider = $this->objects["config"];
+        self::$dataProvider = $this->objects["data"];
+        self::$listener = $this->objects["listener"];
+        
+        self::$main = $this;
+    }
+    
+    /**
+     * @return TouchIt
+     */
+    public static function getTouchIt(){
+        retunr self::$main;
+    }
+    
+    /**
+     * @return SignManager
+     */
+    public static function getManager(){
+        return self::$manager;
+    }
+    
+    /**
+     * @return Provider
+     */
+    public static function getDataProvider(){
+        return self::$dataProvider;
+    }
+    
+    /**
+     * @return Provider
+     */
+    public static function getConfigProvider(){
+        return self::$configProvider;
+    }
+    
+    /**
+     * @return EventListener
+     */
+    public static function getEventListener(){
+        return self::$listener;
+    }
+    
+    /**
+     * @param Provider $object
+     */
+    public function setDataProvider(Provider $object){
+        $this->objects["data"] = $object;
+        self::$dataProvider = $object;
     }
     
     public function onEnable(){
         $this->config->onEnable();
         $this->database->onEnable();
-        $this->signManager->start();
+        $this->signManager->onEnable();
     }
     
     public function onDisable(){
         $this->config->onDisable();
         $this->database->onDisable();
-        $this->signManager->stop();
-    }
-    
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-        return $this->signManager->onCommand(CommandSender $sender, Command $command, $label, array $args);
+        $this->signManager->onDisable();
     }
 }
 ?>
