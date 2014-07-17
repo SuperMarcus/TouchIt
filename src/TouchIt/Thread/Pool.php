26b216b63fde20c7cdc9a84dc958f5cddce2b6f5
Marcus <zhouxule@icloud.com>
2014-07-17 20:42:16 +0800

    Update Pool.php

--- src/TouchIt/Thread/Pool.php
+++ src/TouchIt/Thread/Pool.php
@@ -4,5 +4,10 @@ namespace TouchIt\Pool;
 use TouchIt\Pool\Worker;
 
 class Pool implements Countable, Traversable{
+    private $workers;
+    
+    public function count(){
+        return count($this->workers);
+    }
 }
 ?>
