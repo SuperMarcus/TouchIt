44e8a975c33060c973ac188b008d6e0a3ece2e64
Marcus <zhouxule@icloud.com>
2014-07-18 12:02:44 +0800

    Update some method about thread.

--- src/TouchIt/Thread/Pool.php
+++ src/TouchIt/Thread/Pool.php
@@ -2,12 +2,55 @@
 namespace TouchIt\Pool;
 
 use TouchIt\Pool\Worker;
+use TouchIt\TouchIt;
 
-class Pool implements Countable, Traversable{
-    private $workers;
+class Pool implements Countable{
+    private $workers, $working;
+    
+    public function __construct(){
+        $this->workers = [];
+        $this->working = [];
+    }
+    
+    public function onEnable(){
+        foreach($this->workers as $worker){
+            $worker->startThread();
+        }
+        usleep(20);
+        if(count($this->workers) <= 0){
+            TouchIt::getTouchIt()->getLogger()->debug("[TouchIt] None thread has been start.");
+        }
+    }
+    
+    public function onDisable(){
+        foreach($this->workers as $worker){
+            $worker->stopThread();
+        }
+        usleep(50);
+        if(count($this->working) > 0){
+            TouchIt::getTouchIt()->getLogger()->debug("[TouchIt] There's some thread still working.");
+        }
+    }
+    
+    public function getThreadDescription($id){
+        if(isset($this->working[$id]))return $this->working[$id];
+        else return null;
+    }
+    
+    public function startThread($id, $description = false){
+        $this->working[$id] = $description;
+    }
+    
+    public function stopThread($id){
+        if(isset($this->working[$id])){
+            unset($this->working[$id]);
+        }else{
+            TouchIt::getTouchIt()->getLogger()->debug("[TouchIt] An none-register thread has just stop. Thread ID: ".$id);
+        }
+    }
     
     public function count(){
-        return count($this->workers);
+        return count($this->working);
     }
 }
 ?>
