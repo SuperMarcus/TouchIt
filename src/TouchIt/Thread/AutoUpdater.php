6b3c2f3799501eff846a28cf02478d8a7da20005
Marcus <zhouxule@icloud.com>
2014-07-18 13:05:26 +0800

    Update AutoUpdater.php

--- src/TouchIt/Thread/AutoUpdater.php
+++ src/TouchIt/Thread/AutoUpdater.php
@@ -1,2 +1,33 @@
 <?php
+namespace TouchIt\Thread;
+
+use TouchIt\TouchIt;
+use TouchIt\Exchange\SignData;
+use TouchIt\Thread\Worker;
+
+class AutoUpdater extends Worker implements Countable{
+    public $signs = [];
+    
+    public function count(){
+        return count($this->signs);
+    }
+    
+    public function addSign(SignData $sign){
+        $this->signs[$sign->getId()] = $sign;
+    }
+    
+    public function hasSign($id){
+        return isset($this->signs[((string) $id)]);
+    }
+    
+    public function removeSign($id){
+        
+    }
+    
+    public function onRun(){
+        if(count($this) > 0){
+            
+        }
+    }
+}
 ?>
