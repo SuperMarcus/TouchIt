f85ba4ca55eed636caedeafb81e368ed803e524c
Marcus <zhouxule@icloud.com>
2014-07-18 12:28:47 +0800

    Update new format

--- src/TouchIt/Thread/Worker.php
+++ src/TouchIt/Thread/Worker.php
@@ -2,6 +2,43 @@
 namespace TouchIt\Thread;
 
 use TouchIt\Thread\Pool;
+use TouchIt\TouchIt;
 
-abstract class Worker extends Thread
+abstract class Worker extends \Thread{
+    /** @var Pool */
+    private $pool;
+    
+    private $kill;
+    
+    abstract public function onRun();
+    
+    public function onStop(){}
+    
+    public final function setKillStop($v = true){
+        $this->kill = (bool) $v;
+    }
+    
+    public final function run(){
+        $this->setKillStop();
+        $this->pool->startThread($this->getCurrentThreadId(), get_class($this));
+        $this->onRun();
+        $this->pool->stopThread($this->getCurrentThreadId());
+    }
+    
+    public final function stopThread(){
+        $this->onStop();
+        usleep(70);
+        if($this->isRunning() and $this->kill){
+            $this->kill();
+        }
+    }
+    
+    public final function startThread(){
+        $this->start();
+    }
+    
+    public final function __construct(Pool $pool){
+        $this->pool = $pool;
+    }
+}
 ?>
