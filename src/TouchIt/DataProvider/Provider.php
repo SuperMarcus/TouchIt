1929dc7cd4d94f0e804433337b6005a16e0c5ed0
Marcus <zhouxule@icloud.com>
2014-07-20 12:41:49 +0800

    New provider interface.

--- src/TouchIt/DataProvider/Provider.php
+++ src/TouchIt/DataProvider/Provider.php
@@ -2,8 +2,50 @@
 namespace TouchIt\DataProvider
 
 use TouchIt\TouchIt
+use pocketmine\level\Position;
+use pocketmine\tile\Sign;
 
 interface Provider{
+    /**
+     * Use to get sign from database or filesystem
+     * 
+     * @param Position $pos
+     * 
+     * @return null|array
+     */
+    public function get(Position $pos);
+    
+    /**
+     * Use to get all the sign from database or filesystem
+     * 
+     * @return []
+     */
+    public function getAll();
+    
+    /**
+     * Use to remove sign from database or filesystem
+     * 
+     * @param Position $pos
+     */
+    public function remove(Position $pos);
+    
+    /**
+     * Use to check sign
+     * 
+     * @param Position $pos
+     * 
+     * @return bool
+     */
+    public function exists(Position $pos);
+    
+    /**
+     * Use to add sign to database or filesystem
+     * 
+     * @param Sign $sign
+     */
+    public function create(Sign $sign);
+    
+    /** Load method */
     public function onEnable();
     public function onDisable();
 }
