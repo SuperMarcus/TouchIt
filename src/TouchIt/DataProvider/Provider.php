5f032f5208dfce6dd58a2259eb9d387a6b0c4a01
Marcus <zhouxule@icloud.com>
2014-07-20 12:27:42 +0800

    Update Provider.php

--- src/TouchIt/DataProvider/Provider.php
+++ src/TouchIt/DataProvider/Provider.php
@@ -4,9 +4,6 @@ namespace TouchIt\DataProvider
 use TouchIt\TouchIt
 
 interface Provider{
-    public function isLocked();
-    public function lock();
-    public function unlock();
     public function onEnable();
     public function onDisable();
 }
