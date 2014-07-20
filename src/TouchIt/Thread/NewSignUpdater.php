ea109a0c2afe643fdf57b268782a9a188b426423
Marcus <zhouxule@icloud.com>
2014-07-20 13:12:59 +0800

    Update NewSignUpdater.php

--- src/TouchIt/Thread/NewSignUpdater.php
+++ src/TouchIt/Thread/NewSignUpdater.php
@@ -2,10 +2,39 @@
 namespace TouchIt\Thread;
 
 use TouchIt\Thread\Worker;
+use TouchIt\TouchIt;
 use pocketmine\Tile\Sign;
 use pocketmine\level\Position;
+use pocketmine\Server;
 
 class NewSignUpdater extends Worker{
+    private $startTime, $position, $creater;
     
+    public function __construct(Position $sign, Player $creater){
+        $this->position = $pos;
+        $this->startTime = time();
+        $this->creater = $creater;
+    }
+    
+    public function onRun(){
+        if((time() - $this->startTime) > 5)return true;
+        if((time() - $this->startTime) > TouchIt::getConfigProvider()->get("createTimeout", 60))return false;
+        $tile = $this->position->getLevel()->getTile($this->position);
+        if($tile === false or !($tile instanceof Sign))return false;
+        $text = $tile->getText();
+        if(trim($text[0]) === "" and trim($text[1]) === "" and trim($text[2]) === "" and trim($text[3]) === "")return true;
+        elseif(strtolower(trim($text[0])) === "touchit"){
+            if($this->creater instanceof Player and $this->creater->isOnline()){
+                if(TouchIt::getConfigProvider()->get("AllowPlayerBuild") or $this->creater->isOp()){
+                    if(!Server::getInstance()->isLevelLoaded(trim($text[3])) and TouchIt::getConfigProvider()->get("checkLevel", true)){
+                        $this->creater->sendMessage("[TouchIt] The level you set is not loaded.");
+                    }
+                }else{
+                    $this->creater->sendMessage("[TouchIt] You are not allowed to build this sign.");
+                }
+            }
+        }
+        return false;
+    }
 }
 ?>
