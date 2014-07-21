9a072725402bfe73064e9dc99ecdc1a2907a9e2e
Marcus <zhouxule@icloud.com>
2014-07-21 10:35:09 +0800

    Update SQLite3.sql

--- resources/SQLite3.sql
+++ resources/SQLite3.sql
@@ -1,2 +1,13 @@
-CREATE TABLE sign (
-)
+CREATE TABLE index (
+    id TEXT PRIMARY KEY NOT NULL,
+    type INTEGER NOT NULL
+);
+CREATE TABLE teleport (
+    id TEXT PRIMARY KEY NOT NULL,
+    level TEXT NOT NULL,
+    target TEXT NOT NULL
+);
+CREATE TABLE command (
+    id TEXT PRIMARY KEY NOT NULL,
+    command TEXT NOT NULL,
+);
