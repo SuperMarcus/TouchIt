d3253bda1840650c3fc86ad9a88a9f70894f3a4c
Marcus <zhouxule@icloud.com>
2014-07-24 11:51:36 +0800

    Update ConfigAccessor.php

--- src/TouchIt/ConfigAccessor.php
+++ src/TouchIt/ConfigAccessor.php
@@ -3,8 +3,8 @@ namespace TouchIt;
 
 use TouchIt\TouchIt;
 
-class ConfigAccessor{
-    public $data;
+class ConfigAccessor implements arrayaccess{
+    private $data;
     
     private static $valtype = [
     	"maxPeople" => "intval",
@@ -21,34 +21,62 @@ class ConfigAccessor{
     	$this->analyzeFile();
     }
     
+    public function exists($offset){
+        return isset($this->data[$offset]);
+    }
+    
+    public function get($offset, $default = null){
+        if($this->exists($offset))return $this->data[$offset];
+        else return $default;
+    }
+    
+    public function set($offset, $value){
+        $this->data[$offset] = $value;
+    }
+    
+    public function remove($offset){
+        unset($this->data[$offset]);
+    }
+    
     private function analyzeFile(){
-    	if(!file_exists(TouchIt::getTouchIt()->getDataFolder()."touchit.config")){
-    		file_put_contents(TouchIt::getTouchIt()->getDataFolder()."touchit.config", stream_get_contents(TouchIt::getTouchIt()->getResource("touchit.config")));
+    	if(!file_exists(TouchIt::getTouchIt()->getDataFolder()."config.yml")){
+    		file_put_contents(TouchIt::getTouchIt()->getDataFolder()."config.yml", stream_get_contents(TouchIt::getTouchIt()->getResource("preconfig.yml")));
     	}
-    	$fp = fopen(TouchIt::getTouchIt()->getDataFolder()."touchit.config", "r");
-    	while(!feof($fp)){
-    		$line = fgets($fp);
-    		if($line{0} == "#" or trim($line) == "")continue;
-    		$data = explode("=", $line);
-    		if(isset(self::$valtype[$data[0]])){
-    			if(self::$valtype[$data[0]] === "boolval"){
-    				switch($data[1]){
-    					case "on":
-    					case "true":
-    						$data[1] = true;
-    					case "off":
-    					case "false":
-    						$data[1] = false;
-    					default:
-    						$data[1] = boolval($data[1]);
-    				}
-    			}else{
-    				$data[1] = call_user_func(self::$valtype[$data[0]], $data[1]);
-    			}
-    		}
-    		$this->data[$data[0]] = $data[1];
-    	}
-    	fclose($fp);
+    	$this->data = @yaml_parse(file_get_contents(TouchIt::getTouchIt()->getDataFolder()."config.yml"));
+    }
+    
+    /** Magic methods */
+    public function __get($name){
+        return $this->get($name);
+    }
+    
+    public function __set($name, $value){
+        $this->set($name, $value);
+    }
+    
+    public function __isset($name){
+        return $this->exists($name);
+    }
+    
+    public function __unset($name){
+        return $this->remove($name);
+    }
+    
+    /** Method of ArrayAccess */
+    public function offsetGet($offset){
+        return $this->get($offset);
+    }
+    
+    public function offsetUnset($offset){
+        $this->remove($offset);
+    }
+    
+    public function offsetSet($offset, $value){
+        $this->set($offset, $value);
+    }
+    
+    public function offsetExists($offset){
+        return $this->exists($offset);
     }
 }
 ?>
