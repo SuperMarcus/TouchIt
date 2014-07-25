9c74c19c95d67b7412b3cd877c469c09410e5136
Marcus <zhouxule@icloud.com>
2014-07-25 13:00:04 +0800

    Update BoardCaseSignUpdater.php

--- src/TouchIt/Thread/BoardCaseSignUpdater.php
+++ src/TouchIt/Thread/BoardCaseSignUpdater.php
@@ -2,7 +2,25 @@
 namespace TouchIt;
 
 use TouchIt\TouchIt;
+use TouchIt\Thread\Worker;
+use pocketmine\tile\Sign;
 
-class BoardCaseSignUpdater{
+class BoardCaseSignUpdater extends Worker{
+    public function __construct(){}
+    
+    public function onRun(){
+        $updates = TouchIt::getManager()->needUpdates(TouchIt::SIGN_BOARDCASE);
+        
+        if(count($updates) > 0){
+            foreach($updates as $sign){
+                $pos = $sign['position'];
+                $tile = $pos->getLevel()->getTile($pos);
+                if($tile instanceof Sign){
+                    $massage = TouchIt::getManager()->getAnnouncement();
+                }
+            }
+        }
+        return $this->stop;
+    }
 }
 ?>
