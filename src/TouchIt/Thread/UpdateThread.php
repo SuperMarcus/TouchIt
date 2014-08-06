a6a765c7a24a9f852e150c5a8068d9da5fc54cdd
Marcus <zhouxule@icloud.com>
2014-08-06 12:46:21 +0800

    Update UpdateThread.php

--- src/TouchIt/Thread/UpdateThread.php
+++ src/TouchIt/Thread/UpdateThread.php
@@ -1,2 +1,29 @@
 <?php
+namespace TouchIt\Thread;
+
+use TouchIt\Thread\ThreadManager;
+use pocketmine\tile\Sign;
+
+class UpdateThread extends \Thread{
+    private $thread_manager;
+    private $signs;
+    
+    public function run(){
+        while(count($this->signs) > 0){
+            $sign = @array_shift($this->signs);
+            $tile = $sign['position']->getLevel()->getTile($sign['position']);
+        }
+    }
+    
+    public function __construct(ThreadManager $thread_manager){
+        $this->thread_manager = $thread_manager;
+        $this->signs = [];
+    }
+    
+    public function submit($sign = null){
+        if(is_array($sign)){
+            $this->signs[] = $sign;
+        }
+    }
+}
 ?>
