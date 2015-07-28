7371de2ebe70e3304681925035faa8b0a7558202
Marcus <zhouxule@icloud.com>
2015-07-29 00:24:40 +0800

    Update README.md

--- test/TravisTest.php
+++ test/TravisTest.php
@@ -8,9 +8,17 @@ $server = proc_open(PHP_BINARY . " ../PocketMine.phar --no-wizard --disable-read
 
 fwrite($pipes[0], "version\nmakeplugin TouchIt\nstop\n\n");
 
+$error = 0;
+
 while(!feof($pipes[1])){
+    $line = fgets($pipes[1]);
     echo "[Server] ".fgets($pipes[1]);
+    if(strpos($line, "[CRITICAL]") or strpos($line, "[EMERGENCY]") or strpos($line, "[FATAL]")){
+        echo "[Test] Server output an error message.\n";
+        ++$error;
+    }
 }
+
 fclose($pipes[0]);
 fclose($pipes[1]);
 fclose($pipes[2]);
@@ -18,6 +26,9 @@ fclose($pipes[2]);
 echo "\n\n[Build] Return value: ". proc_close($server) ."\n";
 if(count(glob("../DevTools/TouchIt*.phar")) === 0){
     echo "[Build] No Phar created!\n";
+    exit(1);
+}elseif($error > 0){
+    echo "[Build] There are $error errors during the test.\n";
     exit(1);
 }else{
     echo "[Build] Build successfully ended.\n";
